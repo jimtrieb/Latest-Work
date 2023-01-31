828ffb820bb33c844423f635941b0ba93414b56c
jimtrieb <70408965+jimtrieb@users.noreply.github.com>
2023-01-31 14:19:23 -0600

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,12 +1,3 @@
-/*
-csci 340, assignment 9,fall 2020
-Jim Trieb
-z1912658
-0PE1
-Palak Jalota, Mohith Koppisetti
-11-23-20
-Impliment properties of heaps
-*/
 
 #include <assert.h>
 #include <algorithm>
